ad88c93a89849dbf6de5f498ec9615baeee581b9
mhasilau <mikhail.hasilau@gmail.com>
2022-06-06 13:37:42 +0300

    ref

--- src/app/core/pages/main-page/user-avatar/user-avatar.component.ts
+++ src/app/core/pages/main-page/user-avatar/user-avatar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { noAvatarURL } from '../../../../api/api.config';
 import { UserApiService } from '../../../services/user-api.service';
 import { AvatarService } from '../../../services/avatar.service';
-// import { IUserSignIn } from '../../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-avatar',
@@ -15,9 +14,7 @@ export class UserAvatarComponent implements OnInit{
   constructor(private userApiService: UserApiService, private avatarService: AvatarService) {}
 
   ngOnInit(): void {
-    console.log('ava');
     this.userApiService.user$.subscribe(value => this.uid = value?.id); // TODO communicate subscribes
-    console.log(this.uid);
     this.avatarService.getUserAvatar().subscribe(users => {
       const userLogin = users.find(user => user.id === this.uid);
       this.userAvatar = userLogin!.avatar;
--- src/app/core/pages/sign-in/sign-in.module.ts
+++ src/app/core/pages/sign-in/sign-in.module.ts
@@ -6,18 +6,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    FormsModule,
-  ],
+    imports: [
+        CommonModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        FormsModule,
+        TranslateModule,
+    ],
   exports: [SignInComponent],
 })
 export class SignInModule {}
